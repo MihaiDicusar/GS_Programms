a7eacc62aa0687619371c7f9d94d9ba4a32fd495
YunisMusici <yunis.musici@gmail.com>
2025-11-06 11:17:12 +0100

    Update

--- Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/tmp/TaschenrechnerRPN.Debug+ITSboard-mkdirs.cmake
+++ Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/tmp/TaschenrechnerRPN.Debug+ITSboard-mkdirs.cmake
@@ -4,19 +4,19 @@
 cmake_minimum_required(VERSION 3.5)
 
 file(MAKE_DIRECTORY
-  "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard"
-  "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/1"
-  "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard"
-  "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/tmp"
-  "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp"
-  "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src"
-  "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp"
+  "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard"
+  "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/1"
+  "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard"
+  "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/tmp"
+  "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp"
+  "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src"
+  "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp"
 )
 
 set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
-    file(MAKE_DIRECTORY "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp/${subDir}")
+    file(MAKE_DIRECTORY "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp/${subDir}")
 endforeach()
 if(cfgdir)
-  file(MAKE_DIRECTORY "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp${cfgdir}") # cfgdir has leading slash
+  file(MAKE_DIRECTORY "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/TaschenrechnerRPN.Debug+ITSboard/src/TaschenrechnerRPN.Debug+ITSboard-stamp${cfgdir}") # cfgdir has leading slash
 endif()
--- Programs/TaschenrechnerRPN/tmp/cmake_install.cmake
+++ Programs/TaschenrechnerRPN/tmp/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp
+# Install script for directory: /Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/TaschenrechnerRPN")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/TI_Labor/gitProjekte/ITS-BRD-VSC/Programs/TaschenrechnerRPN/tmp/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/yunis/GitHubRepo/GS_Programms_clone/Programs/TaschenrechnerRPN/tmp/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
